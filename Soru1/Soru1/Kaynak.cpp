f2736f43357d4a4651c25156f45e6a7855e97db3
Umutt0452 <132673835+Umutt0452@users.noreply.github.com>
2023-05-05 15:45:25 +0300

    Soru1

---
+++ Soru1/Soru1/Kaynak.cpp
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+
+int main() {
+    int arr[] = { 10, 20, 20, 10, 10, 20, 5, 20 }; 
+    int n = sizeof(arr) / sizeof(int); 
+    int freq[n]; 
+
+    for (int i = 0; i < n; i++) {
+        freq[i] = 0;
+    }    
+    for (int i = 0; i < n; i++) {
+        freq[arr[i]]++;
+    }
+    for (int i = 0; i < n; i++) {
+        if (freq[i] != 0) {
+            printf("%d →%d\n", i, freq[i]);
+        }
+    }
+    return 0;
+}
